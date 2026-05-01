808bdb0ed91c9c1540c5b7d0c06226fbd7b79291
Mio Yamashita <Mio Yamashita@example.com>
2026-05-01 12:43:19 +0900

    結果で文字の色を変更

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ const fortunes=[
 const button=document.getElementById("omikuji-bt")
 const result=document.getElementById("result")
 
-
 button.addEventListener("click",function(){
-
     const randomNumber=Math.floor(Math.random() * fortunes.length);
+    const resultText=fortunes[(randomNumber)];
 
     result.textContent=fortunes[(randomNumber)];
+
+    if(resultText.includes("大吉")){
+        result.style.color="red";
+    }else if(resultText.includes(中吉)){
+        result.style.color="pink"
+    }else {
+        result.style.color="gray"
+    }
 });
